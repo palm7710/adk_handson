76ee41111480fc9f5b3a41c8bf0c7ff9924b3e1e
Palm <241it002i@s.internet.ac.jp>
2025-09-19 20:27:33 +0900

    agent.py に get_weather 関数を追加します。Agent の name, description, instruction も変更

--- adk_handson/agent.py
+++ adk_handson/agent.py
@@ -2,6 +2,31 @@ import datetime
 from zoneinfo import ZoneInfo
 from google.adk.agents import Agent
 
+# 天気を取得する
+def get_weather(city: str) -> dict:
+    """Retrieves the current weather report for a specified city.
+
+    Args:
+        city (str): The name of the city for which to retrieve the weather report.
+
+    Returns:
+        dict: status and result or error msg.
+    """
+    if city.lower() == "new york":
+        return {
+            "status": "success",
+            "report": (
+                "The weather in New York is sunny with a temperature of 25 degrees"
+                " Celsius (77 degrees Fahrenheit)."
+            ),
+        }
+    else:
+        return {
+            "status": "error",
+            "error_message": f"Weather information for '{city}' is not available.",
+        }
+
+# 現在時刻を取得する
 def get_current_time(tz_identifier: str) -> dict:
     """Returns the current time in a specified timezone identifier.
 
@@ -20,13 +45,13 @@ def get_current_time(tz_identifier: str) -> dict:
     return {"status": "success", "report": report}
 
 root_agent = Agent(
-    name="enjoy_chat_agent",
+    name="weather_time_agent",
     model="gemini-2.5-flash",
     description=(
-        "Agent to chat with user."
+        "Agent to answer questions about the time and weather in a city."
     ),
     instruction=(
         "You are an agent who enjoys chatting with users."
     ),
-    tools=[get_current_time],
+    tools=[get_weather, get_current_time],
 )
